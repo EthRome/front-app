c0faf7b218de8784659438f3b9051e07b2d8c0a4
michal-rozek <michalrozek90@gmail.com>
2024-10-05 12:29:56 +0200

    chore: unauthorized page

--- src/components/pages/AuthorizedPage.tsx
+++ src/components/pages/AuthorizedPage.tsx
@@ -1,16 +1,20 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { LogoutButton } from '../shared/LogoutButton';
+import { Cog6ToothIcon } from '@heroicons/react/24/outline';
 
 export const AuthorizedPage = () => {
   const { user } = useAuth0();
 
   return (
     <div>
-      <div className='w-full h-[235px] bg-gradient rounded-[49px] p-8'>
-        <h3 className='mb-10'>Hello, {user?.given_name}</h3>
+      <div className='w-full h-[235px] flex items-center justify-between bg-gradient rounded-[49px] p-8'>
+        <div>
+          <h3 className='mb-10'>Hello, {user?.given_name}</h3>
+          <button className='w-[18px] h-[18px]'>
+            <Cog6ToothIcon />
+          </button>
+        </div>
       </div>
-
-      <LogoutButton />
+      <div>{/* tutaj tolna część */}</div>
     </div>
   );
 };
