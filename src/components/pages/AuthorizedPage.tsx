394b94a74b336f5618a8cf5eb8aa75683f8076d6
Izabela <izabela.laszczuk@silksh.pl>
2024-10-05 13:04:20 +0200

    styles

--- src/components/pages/AuthorizedPage.tsx
+++ src/components/pages/AuthorizedPage.tsx
@@ -4,6 +4,10 @@ import { Cog6ToothIcon } from '@heroicons/react/24/outline';
 export const AuthorizedPage = () => {
   const { user } = useAuth0();
 
+  const handleOnClick = () => {
+    console.log('Clicked!');
+  };
+
   return (
     <div>
       <div className='w-full h-[235px] flex items-center justify-between bg-gradient rounded-[49px] p-8'>
@@ -14,7 +18,11 @@ export const AuthorizedPage = () => {
           </button>
         </div>
       </div>
-      <div>{/* tutaj tolna część */}</div>
+      <div className='mt-12 mb-4 ml-2 text-xl'>Portfolio</div>
+      <button onClick={handleOnClick} className='w-full h-[80px] flex items-center p-8 rounded-2xl bg-[#281A55]'>
+        <div>Bitcoin</div>
+        <div className='ml-auto'>0.2137</div>
+      </button>
     </div>
   );
 };
